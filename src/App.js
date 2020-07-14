474d48f6a5b8b27fb62c36ec0e62a9723ce9bfff
Michael Loremia <michaelloremia@gmail.com>
2020-07-15 06:21:12 +0800

    added jsx template for handling resume.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as resume from './resume.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{ resume.basics.name }</h1>
+        <p>{ resume.basics.label }</p>
       </header>
+      <content className="App-body">
+        <section>
+          <h3>Contact</h3>
+          <p>Email: { resume.basics.email }</p>
+          <p>Phone: { resume.basics.phone }</p>
+          {
+            resume.basics.profiles.map(p => {
+              return <a key={ p.network } href={ p.url } className={`icon icon-${p.network}`}></a>
+            })
+          }
+        </section>
+        <section>
+          <h3>About</h3>
+          <p>{ resume.basics.summary }</p>
+        </section>
+        <section>
+          <h3>Skills</h3>
+          {
+            resume.skills.map(skill => {
+              return <div>
+                <p>{ skill.name }</p>
+                <ul>
+                  {
+                    skill.keywords.map(hl => {
+                      return <li>{ hl }</li>
+                    })
+                  }
+                </ul>
+              </div>
+            })
+          }
+        </section>
+        <section>
+          <h3>Work Experience</h3>
+          {
+            resume.work.map(w => {
+              return <div key={ w.company }>
+                <h4>{ w.company }</h4>
+                <p>{ w.position } <span>{ `${w.startDate} - ${w.endDate}` }</span></p>
+                <p>{ w.summary }</p>
+                <ul>
+                  {
+                    w.highlights.map( h => {
+                      return <li key={ h }>{ h }</li>
+                    })
+                  }
+                </ul>
+              </div>;
+            })
+          }
+        </section>
+        <section>
+          <h3>Education</h3>
+          {
+            resume.education.map(e => {
+              return <div>
+                  <p>{ `${e.studyType}, ${e.area} - ${e.institution}` }</p>
+                  <p>{`${e.startDate} - ${e.endDate}`}</p>
+                </div>
+            })
+          }
+        </section>
+      </content>
     </div>
   );
 }
